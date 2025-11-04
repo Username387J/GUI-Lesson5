598e2111c2d759d5da6dd03f6aee824263e5fe50
Username387J <jdeleijagbulu@gmail.com>
2025-11-03 19:19:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ root = Tk()
 root.geometry("500x500")
 root.title("Celsius to Farenheit Converter")
 
+def converter():
+    celsius=celsiusentry.get()
+    if(celsius.replace('.',"").isnumeric()):
+        errorlabel.grid_forget()
+        fahrenheit=(float(celsius)*9/5)+32
+        outputlabel.config(text='Tempertture in Fahrenheit: '+str(fahrenheit))
+    else:
+        errorlabel.grid(row=1,column=1)
+
+
+
 titlelabel = Label(root,text="Celsius -> Farenheit",font=font.Font(size=20))
 titlelabel.pack()
 
@@ -20,14 +31,12 @@ celsiusentry.grid(row=0,column=1)
 errorlabel = Label(frame,text="Please enter valid input..")
 #errorlabel.grid(row=1,column=1)
 
-outputlabel = Label(frame,text="Hi")
+outputlabel = Label(frame)
 outputlabel.grid(row=2,column=0)
 
-converterbutton= Button(frame,text="Convert",command=None)
+converterbutton= Button(frame,text="Convert",command=converter)
 converterbutton.grid(row=3,column=0)
 
-
-
-
+ 
 
 root.mainloop()
